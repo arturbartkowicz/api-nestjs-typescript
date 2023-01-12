06530b60438370ba33531eb10b63032b8a97ade1
Artur Bartkowicz <arturbartkowicz@MacBook-Pro-Artur.local>
2023-01-12 11:54:29 +0100

    Add authentication service with methods

---
+++ src/authentication/authentication.service.ts
@@ -0,0 +1,54 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { UsersService } from "src/users/users.service";
+import { RegisterDto } from "./dto/register.dto";
+import * as bcrypt from 'bcrypt';
+import PostgresErrorCode from 'src/database/postgresErrorCode.enum';
+
+export class AuthenticationService {
+  constructor(
+    private readonly usersService: UsersService
+  ){}
+
+  public async register(registrationData: RegisterDto){
+    const hashedPassword = await bcrypt.hash(registrationData.password, 10)
+
+    try{
+      const createdUser =  await this.usersService.create({
+        ...registrationData,
+        password: hashedPassword
+      })
+      // protects for sending a password in a response
+      createdUser.password = undefined
+
+      return createdUser
+    } catch(err) {
+      if (err?.code === PostgresErrorCode.UniqueViolation) {
+        throw new HttpException('User with that email already exists', HttpStatus.BAD_REQUEST);
+      }
+      throw new HttpException('Something went wrong', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  public async getAuthenticatedUser(email: string, plainTextPassword: string){
+    try {
+      const user = await this.usersService.getByEmail(email)
+      await this.verifyPassword(plainTextPassword, user.password)
+      user.password = undefined
+
+      return user
+    } catch(err) {
+      throw new HttpException('Wrong credentials provided', HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  private async verifyPassword(plainTextPassword: string, hashedPassword: string){
+    const isPasswordMatching = await bcrypt.compare(
+      plainTextPassword,
+      hashedPassword
+    )
+
+    if (!isPasswordMatching) {
+      throw new HttpException('Wrong credentials provided', HttpStatus.BAD_REQUEST);
+    }
+  }
+}
